17dd232e03b3259d9b8b014e2a2b9b982fba9c00
Molnar <beianmariana4@gmail.com>
2021-03-02 20:59:24 +0200

    myfirst commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul >
+<a href=" PONTAJ.html">PONTAJ</a><br></br>
+<a href="STAT PLATA.html">STAT PLATA</a><br></br>
+<a href="INTRODUCERE DATE.html">INTRODUCERE DATE</a><br></br>
+<a href="RAPOARTE SALARII.html">RAPOARTE SALARII</a><br></br>
+<a href="ISTORIC.html">ISTORIC</a><br></br>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
